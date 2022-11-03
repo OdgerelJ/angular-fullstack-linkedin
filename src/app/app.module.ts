075dda10a7d75b1782c942805b0d1e2a1628577a
Odgerel Jamsranjav <Odko@BumErdenesiMac.attlocal.net>
2022-11-03 18:12:43 -0500

    initial project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListingPageComponent } from './listing-page/listing-page.component';
+import { ListingDetailPageComponent } from './listings-detail-page/listing-detail-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListingPageComponent,
+    ListingDetailPageComponent,
+    ContactPageComponent,
+    MyListingsPageComponent,
+    NewListingPageComponent,
+    EditListingPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
