8e26c1fe08a004f963d4e90ffd31d219ea84a5d8
Odgerel Jamsranjav <Odko@BumErdenesiMac.attlocal.net>
2022-11-08 21:57:37 -0600

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { ContactPageComponent } from './contact-page/contact-page.component';
 import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
 import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
 import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +24,8 @@ import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
 
   ],
   providers: [],
--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listing-detail-page',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingDetailPageComponent implements OnInit {
 
-  constructor() { }
+  listing: Listing;
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+  ) {
+    this.listing = {
+      id: '',
+      name: '',
+      description: '',
+      price: 0
+    }
   }
 
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id)!;
+  }
 }
